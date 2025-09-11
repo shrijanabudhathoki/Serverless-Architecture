2647fc6b0dc20a9bd007798c737c0ae79780b1be
shrijanabudhathoki <shrijanabudhathoki51@gmail.com>
2025-09-11 08:11:43 +0545

    serverless architecture

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_access_key" {
+  description = "aws access key"
+  type        = string
+  default = "ASIA2HJQR2FQ5MUOPODT"
+}
+
+variable "aws_secret_key" {
+  description = "aws secret key"
+  default = "OJfEpCMviP6r3vysKEg1b9olCFA3zCUnwFXvRqV5"
+  type        = string
+}
+
+variable "aws_session_token" {
+  description = "aws region"
+  type        = string
+  default     = "IQoJb3JpZ2luX2VjEEEaCXVzLWVhc3QtMSJIMEYCIQDp4NaVuZh21AsvQjPxzjKG3Yz58c1ojtgVEH1bbcYANgIhAMrcbpwEgt5TDHMLE3um74mKDLtHaPWjVcFO2JFFMnB7Kq0DCKr//////////wEQABoMNzAyODY1ODU0ODE3Igxy+AOzClNQ5hU+XSsqgQMu79zQKuqCT5UO1mt8ABjxrnA4We6l5bL5XJZWU+VyEcGwRbmkbBqaTyXLZ5bFpfp+crlwT38Z3wjlkV1G33+VGs0w4YZfnrPVpczkt2/Et1diFyHX3PVtXI2x9ni3y8hn8bPMgtWLHgrEWTyaXyRqRq8aFdqgUq1uPOu7MV60H34xqsyPpi3ISbEnXuOIGfoA5YnxTpQBgDg5gdIn32MxtCHp7epelwknYNaLiELLVWSuDezuMzt5v2GZw52C9jVIEAMAU20fm6Sv1S/I22cazmyBfmpI6D1TweKaBR+NPp6UYa45bHFBimpoMXotdad78eV3wCLhZ/SsSJXwPhh7dqLgIpiPdLOzB84pbVzJoVFllrdwgBo5oUsUGE2XU4bugD1wOe6gpfng/ML00n7SPPOzgw3sZujz6PFiKrCUZI5lS4cZODKPwLmKg8zHzZu4+dWS8H3CfPT+Cb1H+LB0J6Ri2R2uPKHmkm5lUYD8mMGRUVQIDzNVVbncTO6AMmfjMOnm9sUGOqUBrvqgj1T+eVzx3aNT3NMxV6QX6jI1s9znpt9S9pmZ10xEfxTEax4oZ0wTv2iSNi/iugwG9020DdLvWZneqNuVNJesb+lvgNaK5WRcb0qXwhAcWusgGNrLuXfGBRi07YQu6yb/3+ghh3oVnVsWKLrWgQUUB029OVDE3+gRK1C5pRhw9VAeV2sd6NonHTBxZ2oJm5W27aa3cPKxJ2q0tcp0Gxbff26P"
+}
+
