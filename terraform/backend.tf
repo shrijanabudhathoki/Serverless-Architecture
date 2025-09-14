1aa2b11e2075c187183681ae086f2fb5570efe22
shrijanabudhathoki <shrijanabudhathoki51@gmail.com>
2025-09-14 12:01:43 +0545

    added backend

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "health-terraform-state-s3"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile = true
+  }
+}
