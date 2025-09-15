dba9872c537ea14b81af11d67a621021cb613468
shrijanabudhathoki <shrijanabudhathoki51@gmail.com>
2025-09-15 13:52:58 +0545

    s3 encryption

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,14 @@ resource "aws_s3_bucket" "input_bucket" {
     Name    = "Shrijana"
     Project = "Serverless Architecture"
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_versioning" "input_bucket_versioning" {
